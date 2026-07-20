e6603ad749515b62ef8fb6fe5b9a0aec253786f0
Dev Bolton <jabolton939@gmail.com>
2026-07-20 19:22:51 -0400

    Began implementation of linear layer activation

--- framework/neuralnet/neuralnet.cpp
+++ framework/neuralnet/neuralnet.cpp
@@ -2,6 +2,34 @@
 #include "../tensor/tensor.hpp"
 #include <iostream>
 
+class Model {
+    private:
+        Tensor<double> data;
+};
+
+class Linear {
+
+    private:
+        Tensor<double> weights_;
+        double bias_;
+
+    public:
+        Linear(size_t inFeats, size_t outFeats, bool bias=true)
+            : weights_({inFeats}, 0.0), bias_(0.5) {
+        }
+
+        template <typename T>
+        Tensor<T> activate(Tensor<T>& x){
+            Tensor<T> y = Tensor<T>::multiply(x, Tensor<T>::transpose(weights_));
+            auto& d = y.data();
+            for(size_t i = 0; i < y.size(); ++i){
+                d[i] += bias_;
+            }
+            return y;
+        }
+
+};
+
 class ReLU {
 
     public:
@@ -19,10 +47,5 @@ class ReLU {
 };
 
 int main(){
-    Tensor x = Tensor({2,2}, 0);
-    x(0,0) = 1;
-    x(0,1) = -2;
-    x(1,0) = 3;
-    x(1,1) = -4;
-    std::cout << x << std::endl;
+
 }
