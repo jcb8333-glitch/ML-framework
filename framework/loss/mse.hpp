e483d456ea2be496bf49314c338e5f50f1652e1e
Jacob Bolton <jabolton939@gmail.com>
2026-07-22 20:24:10 -0400

    Added MSEloss forward

--- framework/loss/mse.hpp
+++ framework/loss/mse.hpp
@@ -11,16 +11,28 @@ class MSEloss{
     public:
         MSEloss() : lastDiff_({1}, T(0)){}
 
-        Tensor<T> loss(Tensor<T>& actual, Tensor<T>& prediction){
+        Tensor<T> forward(Tensor<T>& actual, Tensor<T>& prediction){
             Tensor<T> diff = Tensor<T>::subtract(prediction, actual);
-
-            lastDiff_ = diff;
 
             Tensor<T> square = diff;
             auto& data = square.data();
             for(size_t i = 0; i < data.size(); ++i){
                 data[i] = data[i] * data[i];
             }
+
+            lastDiff_ = diff;
             return Tensor<T>::mean(square);
+        }
+
+        Tensor<T> backward(){
+            Tensor<T> grad = lastDiff_;
+            auto& data = grad.data();
+            T scale = T(2) / T(lastDiff_.size());
+
+            for (size_t i = 0; i < data.size(); ++i){
+                d[i] *= scale;
+            }
+
+            return grad;
         }
 };
